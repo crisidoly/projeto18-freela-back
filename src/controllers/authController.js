025482cc2b65f13aff11d4bce32a23477304898f
crisidoly <cristhiansidoly@gmaill.com>
2023-10-03 18:42:04 -0300

    Auth done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,32 +3,32 @@ import { v4 as uuidv4 } from 'uuid';
 import db from '../database/databaseConnection.js';
 
 export async function SignIn(req, res) {
-    const { email, password } = req.body;
-    const token = uuidv4();
+  const { email, password } = req.body;
+  const token = uuidv4();
 
-    try {
-        const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+  try {
+    const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
 
-        if (!user.rowCount) {
-            return res.status(401).send('Usuário não existe.');
-        }
-
-        const hashedPassword = user.rows[0].password;
-
-        if (!bcrypt.compareSync(password, hashedPassword)) {
-            return res.status(401).send('Senha ou email incorretos.');
-        }
-
-        await db.query('INSERT INTO sessions ("userId", token) VALUES ($1, $2);', [user.rows[0].id, token]);
-
-        res.status(200).send({ token });
-    } catch (err) {
-        res.status(500).send(err.message);
+    if (!user.rowCount) {
+      return res.status(401).send('Usuário não existe.');
     }
+
+    const hashedPassword = user.rows[0].password;
+
+    if (!bcrypt.compareSync(password, hashedPassword)) {
+      return res.status(401).send('Senha ou email incorretos.');
+    }
+
+    await db.query('INSERT INTO sessions ("userId", token) VALUES ($1, $2);', [user.rows[0].id, token]);
+
+    res.status(200).send({ token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 export async function SignUp(req, res) {
-    const { name, email, password, confirmPassword } = req.body;
+    const { name, email, password, confirmPassword, phone, cpf } = req.body;
 
     if (password !== confirmPassword) {
         return res.status(422).send({ message: 'As senhas devem ser iguais!' });
@@ -43,7 +43,7 @@ export async function SignUp(req, res) {
 
         const passwordHash = bcrypt.hashSync(password, 10);
 
-        const newUser = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, passwordHash]);
+        const newUser = await db.query('INSERT INTO users (name, email, password, phone, cpf) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, email, passwordHash, phone, cpf]);
 
         res.status(201).send(newUser.rows[0]);
     } catch (err) {
--- src/schemas/appSchemas.js
+++ src/schemas/appSchemas.js
@@ -1,13 +1,15 @@
 import Joi from "joi";
 
 export const signUpSchema = Joi.object({
-        name: Joi.string().required().min(1),
-        email: Joi.string().required().min(1).email(),
-        password: Joi.string().required(),
-        confirmPassword: Joi.string().required().valid(Joi.ref('password'))
+    name: Joi.string().required().min(1),
+    email: Joi.string().required().min(1).email(),
+    password: Joi.string().required(),
+    confirmPassword: Joi.string().required().valid(Joi.ref('password')),
+    cpf: Joi.string().required(),
+    phone: Joi.string().required()
 });
 
 export const signInSchema = Joi.object({
-        email: Joi.string().required().min(1).email(),
-        password: Joi.string().required().min(1)        
+    email: Joi.string().required().min(1).email(),
+    password: Joi.string().required().min(1)        
 });
